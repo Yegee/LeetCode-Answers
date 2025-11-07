19808ef91cb7fd0b4a4138f493c5274d1e158ac4
Vincent Tran <vincent.tran895@gmail.com>
2025-11-06 18:41:28 -0800

    Comments

--- merge_strings_alternately.py
+++ merge_strings_alternately.py
@@ -1,19 +1,36 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
+'''
+1768. Merge Strings Alternately
+
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+Brute Force(If applicable):
+Loop through each index up to the maximum length of both strings.
+Append characters alternately from word1 and word2 (if available).
+Build the result using simple string concatenation.
+
+Optimal.
+Similar to the brute force, but create an array instead and put into string.
+
+'''
+
+
+def mergeAlternately(self, word1, word2):
+    """
+    :type word1: str
+    :type word2: str
+    :rtype: str
+    """
+    
+    merged = ""
+
+    for i in range(max(len(word1),len(word2))):
+
+        if i < len(word1):
+            merged += word1[i]
         
-        merged = ""
-
-        for i in range(max(len(word1),len(word2))):
-
-            if i < len(word1):
-                merged += word1[i]
-            
-            if i < len(word2):
-                merged += word2[i]
-                        
-        return merged
+        if i < len(word2):
+            merged += word2[i]
+                    
+    return merged
